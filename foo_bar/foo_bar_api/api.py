4598e4d9b102dbf17068cb5cfbfdd9599d3b40d4
Boris Tumbev <b.tumbev@code-nest.com>
2021-02-24 16:29:42 +0200

    auth fix

--- foo_bar/foo_bar_api/api.py
+++ foo_bar/foo_bar_api/api.py
@@ -1,12 +1,10 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from .models import Bar
 from .serializers import BarSerializer
 
 
 class BarList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = BarSerializer
-    # queryset = Bar.objects.all()
 
     def get_queryset(self):
         rating = self.request.query_params.get('rating', 0)
@@ -16,6 +14,5 @@ class BarList(generics.ListCreateAPIView):
 
 
 class BarDetails(generics.RetrieveDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = BarSerializer
     queryset = Bar.objects.all()
